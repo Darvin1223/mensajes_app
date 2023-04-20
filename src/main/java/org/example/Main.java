d21b37abdf5bbb19aa471fe6603579d23857d135
Darvin Rodriguez <darvin@Darvins-MacBook-Pro.local>
2023-04-20 00:14:12 -0400

    Testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
         Conexion conexion = new Conexion();
-
+        String greeting = "hi";
         try(Connection cnx = conexion.get_connection()) {
 
         }catch (Exception e){
